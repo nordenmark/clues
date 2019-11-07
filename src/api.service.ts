5781b50f42d5f93b6728ff064033588326302b73
Nicklas Nordenmark <nordenmark@gmail.com>
2019-11-07 19:52:35 +0100

    Tests fetch

--- src/api.service.ts
+++ src/api.service.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+// import axios from 'axios';
 
 const url =
   process.env.NODE_ENV === 'development'
@@ -11,8 +11,31 @@ interface Response {
 }
 export class ApiService {
   static guess(names: string[]): Promise<Response> {
-    return axios.post<Response>(`${url}/guess`, { names }).then(response => {
-      return response.data;
-    });
+    const options = {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json;charset=UTF-8',
+      },
+      body: JSON.stringify({
+        names,
+      }),
+    };
+
+    return fetch(`${url}/guess`, options).then(response => response.json());
+
+    // return axios
+    //   .post<Response>(
+    //     `${url}/guess`,
+    //     { names },
+    //     {
+    //       headers: {
+    //         'Content-Type': 'application/json',
+    //       },
+    //     }
+    //   )
+    //   .then(response => {
+    //     return response.data;
+    //   });
   }
 }
